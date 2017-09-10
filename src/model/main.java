7351f7aab2f53bff5714e74d1f2d16488a8555a5
Kopanja <31826842+Kopanja@users.noreply.github.com>
2017-09-11 01:20:13 +0200

    Dodat Gui za Konkretne Ture i Profil Korisnika

--- src/model/main.java
+++ src/model/main.java
@@ -1,12 +1,13 @@
 package model;
 import view.MainWindow;
+import view.TuraWindowGui;
 public class main {
 
 	public static void main(String[] args) {
 
 		Application app = new Application();
 		MainWindow mainW = new MainWindow(app.getTure());
-		
+		//TuraWindowGui tGui = new TuraWindowGui(null);
 	}
 
 }
